74409239effa18d9e6421beb3a813af4538aac24
Anton Ruhlov <antonruhlov@gmail.com>
2016-02-21 20:52:33 +0200

    typos

--- am/configfactory/client/base.py
+++ am/configfactory/client/base.py
@@ -31,7 +31,8 @@ class ConfigFactoryClient:
             except Exception as e:
                 logger.warning("Cannot load settings. Using defaults. [{}]".format(str(e)))
         else:
-            logger.warning("Not supported environment. Available environments are: {}.".format(
+            logger.warning("`{}` is supported environment. Available environments are: {}.".format(
+                environment,
                 ', '.join(settings.ENVIRONMENTS)
             ))
 
