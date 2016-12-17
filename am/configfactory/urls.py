011e24388681d3408e5d5c52fe94b966a0d26e20
Anton Ruhlov <anton.ruhlov@admiralmarkets.com>
2016-12-17 14:23:05 +0200

    views cleanup

--- am/configfactory/urls.py
+++ am/configfactory/urls.py
@@ -4,18 +4,18 @@ from am.configfactory import views
 
 
 urlpatterns = [
-    url(r'^$', view=views.ui.index, name='index'),
-    url(r'^backup/dump/$', view=views.ui.backup_dump, name='backup-dump'),
-    url(r'^backup/load/$', view=views.ui.backup_load, name='backup-load'),
-    url(r'^backup/load/(?P<filename>.+)/$', view=views.ui.backup_load, name='backup-load'),
-    url(r'^backup/delete/(?P<filename>.+)/$', view=views.ui.backup_delete, name='backup-delete'),
-    url(r'^components/create/$', view=views.ui.component_create, name='components-create'),
-    url(r'^components/(?P<alias>[-\w\d]+)/$', view=views.ui.component_view, name='components-view'),
-    url(r'^components/(?P<alias>[-\w\d]+)/edit/$', view=views.ui.component_edit, name='components-edit'),
-    url(r'^components/(?P<alias>[-\w\d]+)/edit/schema/$', view=views.ui.component_edit_schema,
+    url(r'^$', view=views.index, name='index'),
+    url(r'^backup/dump/$', view=views.backup_dump, name='backup-dump'),
+    url(r'^backup/load/$', view=views.backup_load, name='backup-load'),
+    url(r'^backup/load/(?P<filename>.+)/$', view=views.backup_load, name='backup-load'),
+    url(r'^backup/delete/(?P<filename>.+)/$', view=views.backup_delete, name='backup-delete'),
+    url(r'^components/create/$', view=views.component_create, name='components-create'),
+    url(r'^components/(?P<alias>[-\w\d]+)/$', view=views.component_view, name='components-view'),
+    url(r'^components/(?P<alias>[-\w\d]+)/edit/$', view=views.component_edit, name='components-edit'),
+    url(r'^components/(?P<alias>[-\w\d]+)/edit/schema/$', view=views.component_edit_schema,
         name='components-edit-schema'),
-    url(r'^components/(?P<alias>[-\w\d]+)/delete/$', view=views.ui.component_delete, name='components-delete'),
-    url(r'^components/(?P<alias>[-\w\d]+)/(?P<environment>\w+)/$', view=views.ui.component_view,
+    url(r'^components/(?P<alias>[-\w\d]+)/delete/$', view=views.component_delete, name='components-delete'),
+    url(r'^components/(?P<alias>[-\w\d]+)/(?P<environment>\w+)/$', view=views.component_view,
         name='components-view'),
 
     url(r'^api/', include('am.configfactory.api.urls'))
--- am/configfactory/views/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import ui
-
-__all__ = [
-    'ui'
-]
