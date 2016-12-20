b56829d05d87de9209779d996e32fe94f264cdc7
Anton Ruhlov <anton.ruhlov@admiralmarkets.com>
2016-12-20 07:45:22 +0200

    api cleanup

--- am/configfactory/api/urls.py
+++ am/configfactory/api/urls.py
@@ -8,24 +8,10 @@ urlpatterns = [
         view=views.components,
         name='api'),
 
-    url(r'^backup/dump/$',
-        view=views.backup_dump,
-        name='api-backup-dump'),
-
-    url(r'^backup/cleanup/$',
-        view=views.backup_cleanup,
-        name='api-backup-cleanup'),
-
     url(r'^(?P<environment>\w+)/$',
-        view=views.components),
-
-    url(r'^(?P<environment>\w+)/get/(?P<path>[-.\w\d]+)/$',
         view=views.components),
 
     url(r'^(?P<environment>\w+)/(?P<alias>[-\w\d]+)/$',
         view=views.component_settings,
         name='api-component'),
-
-    url(r'^(?P<environment>\w+)/(?P<alias>[-\w\d]+)/get/(?P<path>[-.\w\d]+)/$',
-        view=views.component_settings),
 ]
--- am/configfactory/api/views.py
+++ am/configfactory/api/views.py
@@ -3,26 +3,12 @@ from collections import OrderedDict
 from django.conf import settings
 from django.http import Http404, JsonResponse
 from django.shortcuts import get_object_or_404
-from django.views.decorators.http import require_http_methods
 
-from am.configfactory import backup
 from am.configfactory.models import Component
 from am.configfactory.utils import flatten_dict, sort_dict
 
 
-@require_http_methods(["POST"])
-def backup_dump(request):
-    name = backup.dump()
-    return JsonResponse(data=name, safe=False)
-
-
-@require_http_methods(["POST"])
-def backup_cleanup(request):
-    backup.cleanup()
-    return JsonResponse(data=True, safe=False)
-
-
-def components(request, environment=None, path=None):
+def components(request, environment=None):
 
     flatten = _get_flatten_param(request)
     queryset = Component.objects.all()
@@ -39,25 +25,18 @@ def components(request, environment=None, path=None):
             (c.alias, c.get_all_settings()) for c in queryset
         ])
 
-    if path:
-        data = flatten_dict(data)
-        data = data.get(path)
-
     if flatten:
         data = sort_dict(flatten_dict(data))
 
     return JsonResponse(data=data, safe=False)
 
 
-def component_settings(request, environment, alias, path=None):
+def component_settings(request, environment, alias):
 
     component = get_object_or_404(Component, alias=alias)
 
-    flatten = _get_flatten_param(request) or path is not None
+    flatten = _get_flatten_param(request)
     data = component.get_settings(environment)
-
-    if path:
-        data = data.get(path)
 
     if flatten:
         data = sort_dict(flatten_dict(data))
