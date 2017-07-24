76f036453afa4536802b8144cb722d4fde576b59
Anton Ruhlov <anton.ruhlov@admiralmarkets.com>
2017-07-24 14:46:24 +0300

    added better versioning

--- configfactory/__init__.py
+++ configfactory/__init__.py
@@ -1,5 +1,5 @@
-from configfactory.support import get_version
+from configfactory.support.versioning import get_version
 
 # Set public version
-VERSION = '0.2'
+VERSION = '0.3dev'
 __version__ = get_version(VERSION)
